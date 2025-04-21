d02e826b9a29b7c2810186ab84d2af72c071646b
mikail777-1 <vim66333@gmail.com>
2025-04-21 20:54:05 +0300

    Update __init__.py

--- recipes/sqlite3/__init__.py
+++ recipes/sqlite3/__init__.py
@@ -1,8 +1,20 @@
-from pythonforandroid.recipe import CythonRecipe
+from pythonforandroid.recipe import Recipe
 
-class Sqlite3Recipe(CythonRecipe):
+
+class Sqlite3Recipe(Recipe):
     version = '3.35.5'
     url = 'https://www.sqlite.org/2021/sqlite-amalgamation-3350500.zip'
-    name = 'sqlite3'
+    depends = []
+    built_libraries = ['libsqlite3.a']
+
+    def prebuild_arch(self, arch):
+        super().prebuild_arch(arch)
+        # ничего не нужно делать перед сборкой
+
+    def build_arch(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        self.apply_patches(arch)
+        self.build_static_lib(arch, build_dir)
+
 
 recipe = Sqlite3Recipe()
