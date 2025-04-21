0eb75731db40f86269b15c6c803952475510336d
mikail777-1 <vim66333@gmail.com>
2025-04-21 21:39:54 +0300

    Update __init__.py

--- recipes/libffi/__init__.py
+++ recipes/libffi/__init__.py
@@ -1,8 +1,21 @@
-from pythonforandroid.recipe import AutotoolsRecipe
+from pythonforandroid.recipe import Recipe
+import os
 
-class LibffiRecipe(AutotoolsRecipe):
-    version = '3.4.2'
-    url = 'https://github.com/libffi/libffi/releases/download/v{version}/libffi-{version}.tar.gz'
-    name = 'libffi'
+class LibffiRecipe(Recipe):
+    version = "3.4.2"
+    url = "https://github.com/libffi/libffi/releases/download/v{version}/libffi-{version}.tar.gz"
+    name = "libffi"
+
+    def build_arch(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        env = self.get_recipe_env(arch)
+        self.apply_patches(arch)
+        self.ctx.logger.info("Building libffi")
+
+        host = arch.command_prefix.strip("-")  # armeabi-v7a → arm-linux-androideabi
+        configure = f"./configure --host={host} --prefix={build_dir}/install"
+        self.sh(configure, cwd=build_dir, env=env)
+        self.sh("make -j4", cwd=build_dir, env=env)
+        self.sh("make install", cwd=build_dir, env=env)
 
 recipe = LibffiRecipe()
