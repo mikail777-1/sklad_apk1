af14e3440e9c3bd747370ce20e96cb3c1fdc6c5b
mikail777-1 <vim66333@gmail.com>
2025-04-21 21:33:33 +0300

    Update __init__.py

--- recipes/libffi/__init__.py
+++ recipes/libffi/__init__.py
@@ -1,6 +1,6 @@
-from pythonforandroid.recipe import CMakeRecipe
+from pythonforandroid.recipe import AutotoolsRecipe
 
-class LibffiRecipe(CMakeRecipe):
+class LibffiRecipe(AutotoolsRecipe):
     version = '3.4.2'
     url = 'https://github.com/libffi/libffi/releases/download/v{version}/libffi-{version}.tar.gz'
     name = 'libffi'
