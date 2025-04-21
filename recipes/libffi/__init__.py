f1865d5e6aa383d78d389b64367ff8d1271640d8
mikail777-1 <vim66333@gmail.com>
2025-04-21 23:04:53 +0300

    9

--- recipes/libffi/__init__.py
+++ recipes/libffi/__init__.py
@@ -1,30 +1,27 @@
 from pythonforandroid.recipe import Recipe
+from pythonforandroid.util import current_directory, sh
 import os
-import subprocess
 
 
 class LibffiRecipe(Recipe):
-    version = '3.4.2'
-    url = 'https://github.com/libffi/libffi/releases/download/v{version}/libffi-{version}.tar.gz'
-    name = 'libffi'
+    version = "3.3"
+    url = "https://github.com/libffi/libffi/releases/download/v3.3/libffi-3.3.tar.gz"
+    name = "libffi"
 
     def build_arch(self, arch):
-        build_dir = self.get_build_dir(arch.arch)
-        install_dir = os.path.join(self.ctx.get_python_install_dir(arch.arch), 'libffi')
+        build_dir = self.get_build_dir(arch)
+        install_dir = os.path.join(self.ctx.get_python_install_dir(arch), 'libffi')
 
         env = os.environ.copy()
-        env.update(arch.get_env())
+        env['CC'] = self.get_recipe_env(arch)['CC']
+        env['AR'] = self.get_recipe_env(arch)['AR']
+        env['RANLIB'] = self.get_recipe_env(arch)['RANLIB']
 
-        configure = ['./configure',
-                     '--host=arm-linux-androideabi',
-                     f'--prefix={install_dir}']
+        configure = ['./configure', f'--host={arch.command_prefix}', f'--prefix={install_dir}', '--disable-shared', '--enable-static']
+        with current_directory(build_dir):
+            sh(*configure, env=env)
+            sh('make', env=env)
+            sh('make', 'install', env=env)
 
-        if not os.path.exists(os.path.join(build_dir, 'configure')) and os.path.exists(os.path.join(build_dir, 'autogen.sh')):
-            subprocess.run(['./autogen.sh'], cwd=build_dir, env=env, check=True)
-
-        subprocess.run(configure, cwd=build_dir, env=env, check=True)
-        subprocess.run(['make', '-j4'], cwd=build_dir, env=env, check=True)
-        subprocess.run(['make', 'install'], cwd=build_dir, env=env, check=True)
-
-
-recipe = LibffiRecipe()
+    def get_include_dirs(self, arch):
+        return [os.path.join(self.get_build_dir(arch), 'include')]
